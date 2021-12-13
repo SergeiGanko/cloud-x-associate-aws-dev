465adee396f2f60ce2b71fd157cd2258f728cf03
Siarhei Hanko <sergei.ganko@gmail.com>
2021-12-13 23:27:59 +0100

    Simple S3 console app is added

---
+++ CloudX.AwsDev/S3.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Amazon;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using S3.ConsoleApp;
+
+Console.WriteLine("GetLatestObjectVersionByDate");
+var bucketName = "siarhei-hanko-task2";
+var fileName = "file1.txt";
+var profileName = "readAccessS3";
+
+try
+{
+    var sharedCredFile = new SharedCredentialsFile();
+    if (!sharedCredFile.TryGetProfile(profileName, out var profile))
+    {
+        throw new Exception($"Error: couldn't get aws profile: '{profileName}'");
+    }
+
+    AWSCredentialsFactory.TryGetAWSCredentials(profile, sharedCredFile, out var credentials);
+    var s3Client = new AmazonS3Client(credentials, RegionEndpoint.EUWest1);
+
+    var utility = new S3Utility(s3Client, bucketName);
+
+    // get latest file without specifying any date.
+    // Expected result: aLq.VLAEFJmRteXhJQW24R1YTYQrb9Ia
+    //                  file1
+    //                  v3
+    await utility.GetLatestObjectVersionByDateAsync(fileName);
+
+    // get latest file not newer than date = 2021-12-13T20:00:00.
+    // Expected result: DqCNBtiSZjBElnriTelWo8f0Iyg5l1J8,
+    //                  file1,
+    //                  v2
+    var dateString = "2021-12-13T20:00:00";
+
+    if (DateTime.TryParse(dateString, out var date))
+    {
+        await utility.GetLatestObjectVersionByDateAsync(fileName, date);
+    }
+    else
+    {
+        throw new Exception($"Error: couldn't parse string: {dateString} to DateTime");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
---
+++ CloudX.AwsDev/S3.ConsoleApp/S3Utility.cs
@@ -0,0 +1,43 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3.ConsoleApp
+{
+    internal class S3Utility
+    {
+        private readonly AmazonS3Client _client;
+        private readonly string _bucketName;
+
+        public S3Utility(AmazonS3Client client, string bucketName)
+        {
+            _client = client;
+            _bucketName = bucketName;
+        }
+
+        public async Task GetLatestObjectVersionByDateAsync(string fileName, DateTime? date = null)
+        {
+            var listVersions = await _client.ListVersionsAsync(new ListVersionsRequest { BucketName = _bucketName, Prefix = fileName });
+
+            if (!listVersions.Versions.Any())
+            {
+                throw new Exception($"No versions of the file '{fileName}' are found");
+            }
+
+            S3ObjectVersion? latestVersion = date is null
+                ? listVersions.Versions.SingleOrDefault(x => x.IsLatest)
+                : listVersions.Versions.Where(x => x.LastModified < date).OrderByDescending(x => x.LastModified).FirstOrDefault();
+
+            if (latestVersion is null)
+            {
+                throw new Exception($"Latest version of the file '{fileName}' is null");
+            }
+
+            GetObjectRequest request = new GetObjectRequest { BucketName = _bucketName, Key = fileName, VersionId = latestVersion.VersionId };
+
+            using GetObjectResponse getResponse = await _client.GetObjectAsync(request);
+            using StreamReader reader = new StreamReader(getResponse.ResponseStream);
+            Console.WriteLine(latestVersion.VersionId);
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
